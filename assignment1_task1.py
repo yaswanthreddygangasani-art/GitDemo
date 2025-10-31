32cb785d475f5ca8c5de80667adb0eb346c8f2cf
yaswanthreddygangasani-art <yaswanthreddygangasani@gmail.com>
2025-10-31 15:36:38 +0530

    initial commit

---
+++ assignment1_task1.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+add=num1 + num2
+sub=num1 - num2
+mul=num1 * num2
+div=num1 / num2
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
